85d74e3c6a304f927c057ae8e391f6c10e465851
John Lee <drumwolf@Johns-MacBook-Pro.local>
2016-02-21 18:45:55 -0800

    changed some terminology of JS variables

--- main.js
+++ main.js
@@ -1,52 +1,56 @@
 var SlotMachine = function() {
 	this.reels = {};
 	this.$slotMachine = $('#slot-machine');
-	this.$reel   = this.$slotMachine.find('.reel');
-	this.$leverArea = this.$slotMachine.find('.lever-area');
-	this.$lever = this.$slotMachine.find('.lever');
+	this.$reel        = this.$slotMachine.find('.reel');
+	this.$leverArea   = this.$slotMachine.find('.lever-area');
+	this.$lever       = this.$slotMachine.find('.lever');
 	this.$prizeWindow = this.$slotMachine.find('.prize-window');
 	this.init();
 }
 SlotMachine.prototype.init = function() {
-	this.activateButton();
+	this.pullLever();
 }
-SlotMachine.prototype.activateButton = function() {
+SlotMachine.prototype.pullLever = function() {
+	this.$leverArea.on('click',function(){
+		this.$lever.addClass('active');
+		this.spinAllReels();
+	}.bind(this));
+}
+SlotMachine.prototype.spinAllReels = function() {
 	var _this = this;
-	_this.$leverArea.on('click',function(){
-		_this.reels = {};
-		_this.$reel.each(function(){
-			var $reel = $(this);
-			_this.spinReel($reel);
-		});
-		_this.$lever.addClass('active');
-	});
+	this.reels = {};
+	this.$reel.each(function(index, reel){
+		var $reel = $(reel);
+		this.spinReel($reel);
+	}.bind(this));
 }
 SlotMachine.prototype.spinReel = function($reel) {
 	var reelId = $reel.attr('id');
-	var spinCount = 10 + Math.floor(Math.random() * 10);
-	var spinsRemaining = spinCount;
-	while (spinCount) {
-		$reel.animate({ top: "-=66px" }, 50, function(){
+	var spins = 15 + Math.floor(Math.random() * 15);
+	var spinsRemaining = spins;
+	while (spins) {
+		$reel.animate({ top: "-=180px" }, 50, function(){
 			var $first = $reel.find('li').first();
 			$first.detach();
 			$reel.append($first).css({ top: 0 });
 			spinsRemaining--;
 			if (spinsRemaining === 0) {
 				this.reels[reelId] = $reel.find('li').first().data('drink');
-				if (this.allReelsFilled()) {
+				if (this.allReelsFinished()) {
 					this.processResults();
 				}
 			}
 		}.bind(this));
-		spinCount--;
+		spins--;
 	}
 }
-SlotMachine.prototype.allReelsFilled = function() {
+SlotMachine.prototype.allReelsFinished = function() {
 	return (this.reels['maker'] && this.reels['filter'] && this.reels['grounds']);
 }
 SlotMachine.prototype.processResults = function() {
-	this.$lever.removeClass('active');
+	//this.$lever.removeClass('active');
 	console.log(this.reels['maker'] + " / " + this.reels['filter'] + " / " + this.reels['grounds']);
+
 }
 
 // instantiate new instance of SlotMachine
